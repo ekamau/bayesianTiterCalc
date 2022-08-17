5e76014b6728b3e0f283fcade80db8efcce29928
Everlyn Kamau <everlyn.kamau@gmail.com>
2022-08-17 16:00:56 +0100

    Adding tolerance

--- tests/testthat/test-logisticFxn.R
+++ tests/testthat/test-logisticFxn.R
@@ -1,4 +1,4 @@
 test_that("logistic() works", {
   a = 4; b = 0.3; concentration = 12.6;
-  expect_equal(logistic(concentration, a, b), 0.9995822)
+  expect_equal(logistic(concentration, a, b), 0.9995822, tolerance=1e-3)
 })
