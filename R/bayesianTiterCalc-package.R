12d961eba1638a08e2f1d79292e6b246812d0f5b
Everlyn Kamau <everlyn.kamau@gmail.com>
2022-08-22 18:23:28 +0100

    updates to package

--- R/bayesianTiterCalc-package.R
+++ R/bayesianTiterCalc-package.R
@@ -8,7 +8,7 @@
 #' @useDynLib bayesianTiterCalc, .registration = TRUE
 #' @import methods
 #' @import Rcpp
-#' @importFrom rstan sampling
+#' @importFrom rstan sampling optimizing
 #'
 #' @references
 #' Stan Development Team (NA). RStan: the R interface to Stan. R package version 2.26.9. https://mc-stan.org
