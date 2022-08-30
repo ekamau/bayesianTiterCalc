da24316a222b0c63589bf4b684814d6f7aafacae
ben18785 <ben.c.lambert@gmail.com>
2022-08-30 09:46:17 +0100

    updated sample_dose_response

--- tests/testthat/test-sampledoseresponse.R
+++ tests/testthat/test-sampledoseresponse.R
@@ -1,5 +1,5 @@
 test_that("sampledoseresponse function works", {
-  ndraws = 2; a = 8.5; b = 2.5; prior_phi <- list(n=0.75, m=16);
+  ndraws = 2; a = 8.5; b = 2.5; prior_phi <- list(lower=0.75, upper=16);
   dilutions <- 2^c(3, 4, 5, 6, 7, 8, 9, 10);
   simD <- sample_dose_response(ndraws, prior_phi, a, b, dilutions, nreplicates_per_dilution=2);
 
