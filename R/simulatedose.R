e98396010c011adf1f29d10bab68fe50b708e9bc
Everlyn Kamau <everlyn.kamau@gmail.com>
2022-08-31 13:53:06 +0100

    updates to package

--- R/simulatedose.R
+++ R/simulatedose.R
@@ -13,12 +13,13 @@
 #'
 #' @examples
 #' dilutions <- vector()
-#' for(i in 3:10){j <- 2^i; dilutions <- c(dilutions, j)}; phi = 42; a = 4; b = 1.2; nreplicates = 2
+#' for(i in 3:10){j <- 2^i; dilutions <- c(dilutions, j)};
+#' phi = 42; a = 4; b = 1.2; nreplicates = 2
 #' simulate_dose_response(dilutions, phi, a, b, nreplicates)
 #'
-simulate_dose_response <- function(dilutions, phi, a, b, nreplicates=2) {
+simulate_dose_response <- function(dilutions, phi, a, b, nreplicates) {
   survivals <- vector(length = length(dilutions))
   for(i in seq_along(survivals))
     survivals[i] <- simulate_cell_survival(dilutions[i], phi, a, b, nreplicates)
-  tibble(dilution=dilutions, number_surviving=survivals, number_replicates=nreplicates)
+  tibble(dilution = dilutions, number_surviving = survivals, number_replicates = nreplicates)
 }
--- tests/testthat/test-simulatesurvival.R
+++ tests/testthat/test-simulatesurvival.R
@@ -4,7 +4,7 @@ test_that("simulate_cell_survival() works", {
   probability = 1 / (1 + exp(-(a + b * concentration)));
   rbinom(1, nreplicates, probability);
 
-  expect_lt(simulate_cell_survival(dilution, phi, a, b, 2), 3)
+  expect_lt(simulate_cell_survival(dilution, phi, a, b, nreplicates), (nreplicates + 1))
 })
 
 
