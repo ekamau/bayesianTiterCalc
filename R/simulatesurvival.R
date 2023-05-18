58d01c8ab5f5573b47c2fe0edf9e09e64ca7fe41
ben18785 <ben.c.lambert@gmail.com>
2023-05-18 17:36:16 +0100

    Update simulatesurvival.R

--- R/simulatesurvival.R
+++ R/simulatesurvival.R
@@ -4,7 +4,8 @@
 #' @param phi Model estimate.
 #' @param a Model estimate.
 #' @param b Model estimate.
-#' @param nreplicates Fixed value of 2.
+#' @param nreplicates Number of replicates at each dilution (by default value of 2).
+#' @param is_log Is logistic function on log-concentration scale? (by default False).
 #'
 #' @return A numeric value
 #' @export
@@ -14,8 +15,10 @@
 #' dilution = 16; phi = 42; a = 4; b = 1.2; nreplicates = 2
 #' simulate_cell_survival(dilution, phi, a, b, nreplicates)
 #'
-simulate_cell_survival <- function(dilution, phi, a, b, nreplicates) {
+simulate_cell_survival <- function(dilution, phi, a, b, nreplicates, is_log=FALSE) {
   concentration <- phi / dilution
+  if(is_log)
+    concentration <- log(phi / dilution)
   probability <- logistic(concentration, a, b)
   rbinom(1, nreplicates, probability)
 }
